180bb0043741b51a8eda868087f0840598f02aae
mahdiboukhadra <bocamahdi@gmail.com>
2023-10-15 13:13:23 +0100

    first commit

---
+++ visitCounter.py
@@ -0,0 +1,25 @@
+import json
+import boto3
+    
+def lambda_handler(event, context):
+    dynamodb = boto3.resource('dynamodb')
+
+    table = dynamodb.Table('visitcount')
+
+    response = table.get_item(
+        Key={
+            'id':'1'
+        })
+    
+    views = response['Item']['views']
+
+    views = views + 1
+    
+    table.put_item(
+   Item={
+        'id': '1',
+        'views':views
+    }
+)
+    
+    return views
